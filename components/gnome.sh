c49444856eb92eef9af19773d5a9c1f11b628623
Arthur Nunes <arthurnunesc@proton.me>
2023-05-18 00:31:01 +0200

    Change shortcuts

--- components/gnome.sh
+++ components/gnome.sh
@@ -2,7 +2,7 @@
 
 printf "\t- configuring gnome...\n"
 
-# Switch Gnome's stock Alt+tab function to MacOS'
+# Switch Gnome's stock Alt+tab function to Windows'
 dconf write /org/gnome/desktop/wm/keybindings/switch-applications "['']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "['']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<Ctrl>Tab']"
@@ -16,7 +16,7 @@ dconf write /org/gnome/desktop/wm/keybindings/activate-window-menu "['<Alt><Shif
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 
 # Create Super + T shortcut for terminal
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Ctrl>t'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Ctrl><Alt>t'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'kitty'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Open Kitty terminal emulator'"
 # Create Ulauncher shortcut
@@ -28,19 +28,18 @@ dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/cus
 dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:swap_lalt_lctl']"
 
 # Change fonts
-dconf write /org/gnome/desktop/interface/font-name "'Inter Regular 11'"
+dconf write /org/gnome/desktop/interface/font-name "'Cantarell Regular 11'"
 dconf write /org/gnome/desktop/interface/document-font-name "'Sans Regular 11'"
-dconf write /org/gnome/desktop/interface/monospace-font-name "'Fira Code 11'"
-dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Inter Bold 11'"
+dconf write /org/gnome/desktop/interface/monospace-font-name "'IntelOne Mono 11'"
+dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Sans Bold 10'"
 
 # Change format to Botswana(the same as Brazil but in English)
 dconf write /system/locale/region "'en_BW.UTF-8'"
 
-# Change touchpad configurations
+# Change touchpad and mouse configurations
 dconf write /org/gnome/desktop/peripherals/touchpad/natural-scroll true
 dconf write /org/gnome/desktop/peripherals/touchpad/two-finger-scrolling-enabled true
 dconf write /org/gnome/desktop/peripherals/touchpad/tap-to-click true
-# Disable mouse acceleration
 dconf write /org/gnome/desktop/peripherals/mouse/accel-profile "'flat'"
 dconf write /org/gnome/desktop/peripherals/mouse/natural-scroll true
 
@@ -66,15 +65,15 @@ dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
 dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
 
 # Change locking key
-dconf write /org/gnome/settings-daemon/plugins/media-keys/screensaver "'<Alt><Ctrl>q'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/screensaver "'<Ctrl><Alt>q'"
+
+# Disable telemetry
+dconf write /org/gnome/desktop/privacy/report-technical-problems false
 
 # UNUSED CONFIGS #
 
 # # Turn on over-amplification
 # dconf write /org/gnome/desktop/sound/allow-volume-above-100-percent false
-
-# # Disable telemetry
-# dconf write /org/gnome/desktop/privacy/report-technical-problems false
 
 # # Change scaling
 # dconf write /org/gnome/desktop/interface/text-scaling-factor "1.45"
