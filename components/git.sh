aa15ae203b1aa509aad8f4eee1474381b1be1bb0
Arthur Nunes <arthurnunesc@proton.me>
2023-12-08 15:48:36 +0100

    Change Projects folder naming to Developer

--- components/git.sh
+++ components/git.sh
@@ -6,7 +6,7 @@ git config --global user.email "arthurnunesc@proton.me"
 git config --global user.name "Arthur Nunes"
 git config --global core.editor "nvim"
 
-# Create Projects folder
-if [ ! -d "$HOME/Projects" ]; then
-  mkdir "$HOME"/Projects
+# Create Developer folder
+if [ ! -d "$HOME/Developer" ]; then
+  mkdir "$HOME"/Developer
 fi
